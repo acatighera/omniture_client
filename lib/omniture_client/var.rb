790a2ea81797c2d185e064040f0fa25a3bb85f54
alex <alex@patch.com>
2009-12-08 15:31:41 -0500

    ensure that variable name is always a string part 2

--- lib/omniture_client/var.rb
+++ lib/omniture_client/var.rb
@@ -2,11 +2,10 @@ module OmnitureClient
   class Var
     attr_reader :name, :value
     def initialize(name, value)
-      @name = name.to_s
-      if OmnitureClient::aliases && OmnitureClient::aliases[name]
-        @name = OmnitureClient::aliases[name]
+      if OmnitureClient::aliases && OmnitureClient::aliases[name.to_s]
+        @name = OmnitureClient::aliases[name.to_s]
       else
-        @name = name
+        @name = name.to_s
       end
       @value = value
     end
