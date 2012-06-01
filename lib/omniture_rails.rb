d0282af32650664befad17da104824d943cd931f
Vitaly Tsevan <vzevan@gmail.com>
2012-06-01 12:58:00 +0300

    fixed bug. there were only flash errors after redirect to page with reporter

--- lib/omniture_rails.rb
+++ lib/omniture_rails.rb
@@ -2,43 +2,58 @@ module OmnitureClient
   module ActionControllerMethods
 
     def self.included(base)
+      base.send(:before_filter, :report_omniture_if_events_in_flash)
       base.extend(ClassMethods)
+      base.send(:include, InstanceMethods)
+      attr_accessor :reporter
     end
 
     module ClassMethods
       def reports_to_omniture(options = {})
-        include InstanceMethods
         before_filter :set_reporter, options
-        before_filter :assign_flash_vars, options
-        attr_accessor :reporter
       end
     end
 
     module InstanceMethods
+
       def omniture_flash
-        flash[:omniture] ||= {}
+        @omniture_flash ||= (flash[:omniture] || {})
       end
 
       def omniture_url
         ssl = :ssl if request.ssl? && OmnitureClient::ssl_url
-        reporter.url(ssl)
-      end      
+        self.reporter.url(ssl)
+      end
 
       private
 
       def set_reporter
-        @reporter ||= begin
-          "#{controller_path.classify}Reporter".constantize.new(self)
-         rescue NameError
+        self.reporter = select_reporter
+        report_omniture_if_events_in_flash
+      end
+
+      def select_reporter
+        begin
+           "#{controller_path.classify}Reporter".constantize.new(self)
+        rescue NameError
            BasicReporter.new(self)
-         end        
+        end
+      end
+
+      def report_omniture_if_events_in_flash
+        if flash[:omniture] && flash[:omniture][:events]
+          if self.class.respond_to?(:reports_to_omniture) && defined?(self.class::OMNITURE_ACTIONS) && !self.class::OMNITURE_ACTIONS.include?(action_name)
+            self.reporter ||= select_reporter
+            assign_flash_vars
+          end
+        end
       end
 
       def assign_flash_vars
-        reporter.class.clear_meta_vars unless omniture_flash.empty?
         omniture_flash.each do |name, value|
-          reporter.add_var(name, value)
-        end        
+          self.reporter.add_var(name, value)
+        end
+        flash[:omniture].clear if omniture_flash.present?
       end
     end
   end
